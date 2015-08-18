ae12f0162ab9ec11577092480d7a7fac6a57c6e4
ppworks <koshikawa@ppworks.jp>
2015-08-19 00:15:19 +0900

    Initial Commit

---
+++ circleaf-iosTests/circleaf_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  circleaf_iosTests.swift
+//  circleaf-iosTests
+//
+//  Created by koshikawa on 2015/08/19.
+//  Copyright (c) 2015年 ppworks. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class circleaf_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
